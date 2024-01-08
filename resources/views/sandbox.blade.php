ff4fe20f4a8bf3f6e564cc0c68e0f588b83f9ceb
Iustin Ghergu <iusting20@gmail.com>
2024-01-08 17:19:08 +0000

    Initial commit

---
+++ resources/views/sandbox.blade.php
@@ -0,0 +1,17 @@
+<html>
+    <head>
+        
+    </head>
+
+    <body style="background-color: #181A1B; margin: 0px; padding: 20px; border: 5px solid green; color: white;">
+        <form action="/your-processing-url" method="post">
+            <label for="name">Name:</label>
+            <input type="text" id="name" name="name"><br><br>
+
+            <label for="email">Email:</label>
+            <input type="email" id="email" name="email"><br><br>
+
+            <input type="submit" value="Submit">
+        </form>
+    </body>
+</html>
