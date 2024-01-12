2508a13ca6b9931b5b162a0dce5d3485df9e11eb
Iustin Ghergu <iusting20@gmail.com>
2024-01-12 14:41:44 +0000

    added form field validation on the front-end (client-side)

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -4,39 +4,59 @@
 
     <div>
         <p>*Please fill in all fields before submitting the form.</p>
+        <div v-if="dataSubmitted" style="color: green">
+            Data has been submitted successfully!
+        </div>
 
         <!-- name, email, phone, and address -->
         <label for="name">Name:</label>
         <input v-model="name" type="text" id="name" />
+        <div v-if="v$.name.$error && !dataSubmitted" style="color: red">
+            Name field has an error: {{ v$.name.$errors[0].$message }}
+        </div>
         <br />
 
         <label for="email">Email:</label>
         <input v-model="email" type="email" id="email" />
+        <div v-if="v$.email.$error && !dataSubmitted" style="color: red">
+            Email field has an error: {{ v$.email.$errors[0].$message }}
+        </div>
         <br />
 
         <label for="phone">Phone No.:</label>
         <input v-model="phone" type="text" id="phone" />
+        <div v-if="v$.phone.$error && !dataSubmitted" style="color: red">
+            Phone no. field has an error: {{ v$.phone.$errors[0].$message }}
+        </div>
         <br />
 
         <label for="address">Address:</label>
         <input v-model="address" type="text" id="address" />
+        <div v-if="v$.address.$error && !dataSubmitted" style="color: red">
+            Address field has an error: {{ v$.address.$errors[0].$message }}
+        </div>
         <br />
 
         <!-- make button clickable only when all fields are filled in -->
-        <button @click="addUser" :disabled="!allFieldsAreFilledIn">
-            Add User
-        </button>
+        <button @click="addUser">Add User</button>
     </div>
 </template>
 <script>
+import { useVuelidate } from "@vuelidate/core";
+import { required, email, numeric } from "@vuelidate/validators";
+import { ErrorCodes } from "vue";
+
 export default {
-    setup() {},
+    setup() {
+        return { v$: useVuelidate() };
+    },
     data() {
         return {
             name: "",
             email: "",
             phone: "",
             address: "",
+            dataSubmitted: false,
         };
     },
     computed: {
@@ -46,27 +66,37 @@ export default {
     },
     methods: {
         addUser() {
-            axios
-                .post("/api/add_customer", {
-                    name: this.name,
-                    email: this.email,
-                    phone: this.phone,
-                    address: this.address,
-                })
-                .then((response) => {
-                    // handle successful response
-                    console.log(response.data);
+            // initialise prompt
+            this.dataSubmitted = false;
 
-                    // display success message and clear form, etc.
-                    // ...
-                    this.clearForm();
-                })
-                .catch((error) => {
-                    // log error
-                    console.error(error);
-                    // display error message to UI
-                    // ...
-                });
+            // validates fields
+            this.v$.$validate();
+
+            // if there are no validation errors send call to API
+            if (this.v$.$errors.length == 0) {
+                axios
+                    .post("/api/add_customer", {
+                        name: this.name,
+                        email: this.email,
+                        phone: this.phone,
+                        address: this.address,
+                    })
+                    .then((response) => {
+                        // handle successful response
+                        console.log(response.data);
+
+                        // display success message and clear form, etc.
+                        // ...
+                        this.clearForm();
+                        this.dataSubmitted = true;
+                    })
+                    .catch((error) => {
+                        // log error
+                        console.error(error);
+                        // display error message to UI
+                        // ...
+                    });
+            }
         },
         clearForm() {
             this.name = "";
@@ -74,6 +104,14 @@ export default {
             this.phone = "";
             this.address = "";
         },
+    },
+    validations() {
+        return {
+            name: { required },
+            email: { required, email },
+            phone: { required, numeric },
+            address: { required },
+        };
     },
 };
 </script>
