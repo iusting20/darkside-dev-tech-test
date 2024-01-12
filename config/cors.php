8a3e926018ee8f8375fe3fce1d30761c98e03d47
Iustin Ghergu <iusting20@gmail.com>
2024-01-12 15:21:47 +0000

    added comment to CORS config file and limited Allowed Methods to GET & POST

--- config/cors.php
+++ config/cors.php
@@ -17,9 +17,9 @@ return [
 
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
 
-    'allowed_methods' => ['*'],
+    'allowed_methods' => ['GET', 'POST'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => ['*'],  // update this when online on a .co.uk or .com domain
 
     'allowed_origins_patterns' => [],
 
